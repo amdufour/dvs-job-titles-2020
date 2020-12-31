277a7b5463a2af6ba1dfc8212bbd959b9580e62e
Anne-Marie Dufour <hello@annemariedufour.com>
2020-12-31 13:26:24 +0100

    Add color variables

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+// Variables
+
+// Colors
+const redDark = '#903227';
+const redMedium = '#CC5A4B';
+const redPale = '#DB8A80';
+
+const greenDark = '#355F30';
+const greenMedium = '#598235';
+const greenPale = '#95AD5C';
+
+const yellowGold = '#C88304';
+const yellowMedium = '#FAA80F';
+const yellowPale = '#FCC55F';
+
+const blueDark = '#295266';
+const blueMedium = '#407FA0';
+const bluePale = '#6DA8C5';
