99015f97d10e17a2047c76002ef4adabb139712d
Anne-Marie Dufour <hello@annemariedufour.com>
2021-01-01 13:22:28 +0100

    Fix data fields detection logic

--- src/js/clean_data.js
+++ src/js/clean_data.js
@@ -3,6 +3,7 @@ let data = [];
 const sections = [
   { sct_id: 'data_science_sct', groups: ['data_specialist', 'bi', 'analyst', 'analytics', 'librarian', 'geo', 'dataviz'] },
 ];
+const fields = ['field_ds', 'field_it', 'field_science', 'field_art', 'field_design', 'field_communications', 'field_finance', 'field_business', 'field_economy', 'field_student', 'field_teacher', 'field_research', 'field_unknown'];
 
 sections.forEach(sct => {
   let currentSct = {};
@@ -34,20 +35,9 @@ d3.csv('../data/data_job_titles.csv', (data_original) => {
   group.sumPeople += jobTitleInfo.num_people;
 
   jobTitleInfo.fields = [];
-  if (data_original.field_viz === '1') { jobTitleInfo.fields.push('field_viz'); }
-  if (data_original.field_ds === '1') { jobTitleInfo.fields.push('field_ds'); }
-  if (data_original.field_it === '1') { jobTitleInfo.fields.push('field_it'); }
-  if (data_original.field_science === '1') { jobTitleInfo.fields.push('field_science'); }
-  if (data_original.field_art === '1') { jobTitleInfo.fields.push('field_art'); }
-  if (data_original.field_design === '1') { jobTitleInfo.fields.push('field_design'); }
-  if (data_original.field_communications === '1') { jobTitleInfo.fields.push('field_communications'); }
-  if (data_original.field_finance === '1') { jobTitleInfo.fields.push('field_finance'); }
-  if (data_original.field_business === '1') { jobTitleInfo.fields.push('field_business'); }
-  if (data_original.field_economy === '1') { jobTitleInfo.fields.push('field_economy'); }
-  if (data_original.field_student === '1') { jobTitleInfo.fields.push('field_student'); }
-  if (data_original.field_teacher === '1') { jobTitleInfo.fields.push('field_teacher'); }
-  if (data_original.field_research === '1') { jobTitleInfo.fields.push('field_research'); }
-  if (data_original.field_unknown === '1') { jobTitleInfo.fields.push('field_unknown'); }
+  fields.forEach(field => {
+    if (data_original[field] === '1') { jobTitleInfo.fields.push(field); }
+  });
   group.titles.push(jobTitleInfo);
 
   if (data_original.pos_senior !== '') { group.sumSeniors += parseInt(data_original.pos_senior); }
