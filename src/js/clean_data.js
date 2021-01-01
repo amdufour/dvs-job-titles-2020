1726094a4f7b546c4548a2390fb474620d45fd6c
Anne-Marie Dufour <hello@annemariedufour.com>
2021-01-01 14:16:26 +0100

    Add color mix function

--- src/js/clean_data.js
+++ src/js/clean_data.js
@@ -3,7 +3,6 @@ let data = [];
 const sections = [
   { sct_id: 'data_science_sct', groups: ['data_specialist', 'bi', 'analyst', 'analytics', 'librarian', 'geo', 'dataviz'] },
 ];
-const fields = ['field_ds', 'field_it', 'field_science', 'field_art', 'field_design', 'field_communications', 'field_finance', 'field_business', 'field_economy', 'field_student', 'field_teacher', 'field_research', 'field_unknown'];
 
 sections.forEach(sct => {
   let currentSct = {};
@@ -36,9 +35,13 @@ d3.csv('../data/data_job_titles.csv', (data_original) => {
 
   jobTitleInfo.fields = [];
   fields.forEach(field => {
-    if (data_original[field] === '1') { jobTitleInfo.fields.push(field); }
+    if (data_original[field.field_id] === '1') { jobTitleInfo.fields.push(field.field_id); }
   });
   group.titles.push(jobTitleInfo);
+  
+  jobTitleInfo.color = jobTitleInfo.fields.length === 1
+    ? fields.find(field => field.field_id === jobTitleInfo.fields[0]).field_color
+    : mixColors(jobTitleInfo.fields);
 
   if (data_original.pos_senior !== '') { group.sumSeniors += parseInt(data_original.pos_senior); }
   if (data_original.pos_lead !== '') { group.sumLeaders += parseInt(data_original.pos_lead); }
